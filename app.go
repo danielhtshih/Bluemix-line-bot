abca25129c467d8d187d40e7ebedfdefa8cd2400
Daniel Shih <hotingwow@gmail.com>
2017-02-02 15:14:14 +0800

    replace app.go to line bot example: https://github.com/line/line-bot-sdk-go/blob/master/examples/echo_bot/server.go

--- app.go
+++ app.go
@@ -1,36 +1,61 @@
+// Copyright 2016 LINE Corporation
+//
+// LINE Corporation licenses this file to you under the Apache License,
+// version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at:
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 package main
 
 import (
 	"log"
 	"net/http"
 	"os"
-	"html/template"
-	//for extracting service credentials from VCAP_SERVICES
-	//"github.com/cloudfoundry-community/go-cfenv"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
-
-const (
-	DEFAULT_PORT = "8080"
-)
-
-var index = template.Must(template.ParseFiles(
-  "templates/_base.html",
-  "templates/index.html",
-))
-
-func helloworld(w http.ResponseWriter, req *http.Request) {
-  index.Execute(w, nil)
-}
 
 func main() {
-	var port string
-	if port = os.Getenv("PORT"); len(port) == 0 {
-		port = DEFAULT_PORT
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", helloworld)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	
-	log.Printf("Starting app on port %+v\n", port)
-	http.ListenAndServe(":"+port, nil)
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
 }
